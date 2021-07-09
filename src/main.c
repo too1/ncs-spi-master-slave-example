9b5e8e280017ca63db6db1b1a3a9a0736053e158
Torbjørn Øvrebekk <torbjorn.ovrebekk@nordicsemi.no>
2021-07-09 15:10:30 +0200

    SPI slave code added

--- src/main.c
+++ src/main.c
@@ -28,17 +28,16 @@
 #define FLAGS	0
 #endif
 
+// SPI master functionality
 const struct device *spi_dev;
-struct k_poll_signal spi_done_sig;
+static struct k_poll_signal spi_done_sig = K_POLL_SIGNAL_INITIALIZER(spi_done_sig);
 
 static void spi_init(void)
 {
-	spi_dev = device_get_binding("SPI_2");
+	spi_dev = device_get_binding("SPI_1");
 	if(spi_dev == NULL){
 		printk("Error getting SPI master device!!\n");
 	}
-
-	k_poll_signal_init(&spi_done_sig);
 }
 
 static const struct spi_config spi_cfg = {
@@ -88,11 +87,84 @@ static int spi_write_test_msg(void)
 	}
 
 	// Wait for the done signal to be raised and log the rx buffer
-	k_sleep(K_MSEC(10));
+	int spi_signaled, spi_result;
+	do{
+		k_poll_signal_check(&spi_done_sig, &spi_signaled, &spi_result);
+	} while(spi_signaled == 0);
 	printk("SPI RX: 0x%.2x, 0x%.2x\n", rx_buffer[0], rx_buffer[1]);
 	return 0;
 }
 
+// SPI slave functionality
+const struct device *spi_slave_dev;
+static struct k_poll_signal spi_slave_done_sig = K_POLL_SIGNAL_INITIALIZER(spi_slave_done_sig);
+
+static const struct spi_config spi_slave_cfg = {
+	.operation = SPI_WORD_SET(8) | SPI_TRANSFER_MSB |
+		     SPI_MODE_CPOL | SPI_MODE_CPHA | SPI_OP_MODE_SLAVE,
+	.frequency = 4000000,
+	.slave = 0,
+};
+
+static void spi_slave_init(void)
+{
+	spi_slave_dev = device_get_binding("SPI_0");
+	if(spi_slave_dev == NULL){
+		printk("Error getting SPI slave device!!\n");
+	}
+}
+
+static uint8_t slave_tx_buffer[2];
+static uint8_t slave_rx_buffer[2];
+static int spi_slave_write_test_msg(void)
+{
+	static uint8_t counter = 0;
+
+
+	const struct spi_buf s_tx_buf = {
+		.buf = slave_tx_buffer,
+		.len = sizeof(slave_tx_buffer)
+	};
+	const struct spi_buf_set s_tx = {
+		.buffers = &s_tx_buf,
+		.count = 1
+	};
+
+	struct spi_buf s_rx_buf = {
+		.buf = slave_rx_buffer,
+		.len = sizeof(slave_rx_buffer),
+	};
+	const struct spi_buf_set s_rx = {
+		.buffers = &s_rx_buf,
+		.count = 1
+	};
+
+	// Update the TX buffer with a rolling counter
+	slave_tx_buffer[1] = counter++;
+	printk("SPI SLAVE TX: 0x%.2x, 0x%.2x\n", slave_tx_buffer[0], slave_tx_buffer[1]);
+
+	// Reset signal
+	k_poll_signal_reset(&spi_slave_done_sig);
+	
+	// Start transaction
+	int error = spi_transceive_async(spi_slave_dev, &spi_slave_cfg, &s_tx, &s_rx, &spi_slave_done_sig);
+	if(error != 0){
+		printk("SPI slave transceive error: %i\n", error);
+		return error;
+	}
+
+	return 0;
+}
+
+static int spi_slave_check_for_message(void)
+{
+	int signaled, result;
+	k_poll_signal_check(&spi_slave_done_sig, &signaled, &result);
+	if(signaled != 0){
+		return 0;
+	}
+	else return -1;
+}
 
 void main(void)
 {
@@ -112,12 +184,24 @@ void main(void)
 
 	spi_init();
 
+	spi_slave_init();
+
 	printk("SPI master/slave example started\n");
+
+	spi_slave_write_test_msg();
 
 	while (1) {
 		spi_write_test_msg();
 		gpio_pin_set(dev, PIN, (int)led_is_on);
 		led_is_on = !led_is_on;
 		k_msleep(SLEEP_TIME_MS);
+
+		if(spi_slave_check_for_message() == 0){
+			// Print the last received data
+			printk("SPI SLAVE RX: 0x%.2x, 0x%.2x\n", slave_rx_buffer[0], slave_rx_buffer[1]);
+			
+			// Prepare the next SPI slave transaction
+			spi_slave_write_test_msg();
+		}
 	}
 }
