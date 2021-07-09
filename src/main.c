1b6eb6809d8f828a5c5e1c7cc04598e30377235b
Torbjørn Øvrebekk <torbjorn.ovrebekk@nordicsemi.no>
2021-07-09 11:28:35 +0200

    SPI master added

--- src/main.c
+++ src/main.c
@@ -8,6 +8,7 @@
 #include <device.h>
 #include <devicetree.h>
 #include <drivers/gpio.h>
+#include <drivers/spi.h>
 
 /* 1000 msec = 1 sec */
 #define SLEEP_TIME_MS   1000
@@ -27,6 +28,72 @@
 #define FLAGS	0
 #endif
 
+const struct device *spi_dev;
+struct k_poll_signal spi_done_sig;
+
+static void spi_init(void)
+{
+	spi_dev = device_get_binding("SPI_2");
+	if(spi_dev == NULL){
+		printk("Error getting SPI master device!!\n");
+	}
+
+	k_poll_signal_init(&spi_done_sig);
+}
+
+static const struct spi_config spi_cfg = {
+	.operation = SPI_WORD_SET(8) | SPI_TRANSFER_MSB |
+		     SPI_MODE_CPOL | SPI_MODE_CPHA,
+	.frequency = 4000000,
+	.slave = 0,
+};
+
+
+static int spi_write_test_msg(void)
+{
+	static uint8_t counter = 0;
+	static uint8_t tx_buffer[2];
+	static uint8_t rx_buffer[2];
+
+	const struct spi_buf tx_buf = {
+		.buf = tx_buffer,
+		.len = sizeof(tx_buffer)
+	};
+	const struct spi_buf_set tx = {
+		.buffers = &tx_buf,
+		.count = 1
+	};
+
+	struct spi_buf rx_buf = {
+		.buf = rx_buffer,
+		.len = sizeof(rx_buffer),
+	};
+	const struct spi_buf_set rx = {
+		.buffers = &rx_buf,
+		.count = 1
+	};
+
+	// Update the TX buffer with a rolling counter
+	tx_buffer[0] = counter++;
+	printk("SPI TX: 0x%.2x, 0x%.2x\n", tx_buffer[0], tx_buffer[1]);
+
+	// Reset signal
+	k_poll_signal_reset(&spi_done_sig);
+	
+	// Start transaction
+	int error = spi_transceive_async(spi_dev, &spi_cfg, &tx, &rx, &spi_done_sig);
+	if(error != 0){
+		printk("SPI transceive error: %i\n", error);
+		return error;
+	}
+
+	// Wait for the done signal to be raised and log the rx buffer
+	k_sleep(K_MSEC(10));
+	printk("SPI RX: 0x%.2x, 0x%.2x\n", rx_buffer[0], rx_buffer[1]);
+	return 0;
+}
+
+
 void main(void)
 {
 	const struct device *dev;
@@ -43,7 +110,12 @@ void main(void)
 		return;
 	}
 
+	spi_init();
+
+	printk("SPI master/slave example started\n");
+
 	while (1) {
+		spi_write_test_msg();
 		gpio_pin_set(dev, PIN, (int)led_is_on);
 		led_is_on = !led_is_on;
 		k_msleep(SLEEP_TIME_MS);
